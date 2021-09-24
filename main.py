78457803b4f970b8ac9eb19e559baff6d16d9b24
Mario Cordero <mcorderoulate@gmail.com>
2021-09-23 22:32:09 -0600

    Codificacion de las primeras lineas de codigo

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hola mundo")
+print("BIENVENIDO AL CAJERO AUTOMATICO DEL BANCO BMC")
+saldo = 100000;
+Retiro = 0;
+MontoFinal = 0;
+Pin = 1234;
+
+Pin2 = int(input("Bienvenido por favor ingrese su numero de PIN \n"))
+if(Pin2 == Pin)
+    Retiro = int(print(""))
